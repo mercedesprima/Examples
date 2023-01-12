ba10d45ae4ebcb99b8f475997c4cab80faae5eb4
Ekaterina_P <zazie25@yandex.ru>
2023-01-12 17:47:53 +0300

    add Code start part 1

---
+++ Code.start.part.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача. Задать массив из N положительных чисел и найти в нем сумму двухзначных элементов. 
+int[] elements = new int[10];
+
+int length = elements.Length;
+int index = 0;
+int sum = 0;
+
+while (index < length)
+{
+    int current = Random.Shared.Next(30);
+    elements[index] = current;
+
+Console.WriteLine(elements[index]);
+
+    if (current >= 10 && current <= 99)
+    { 
+        sum = sum + current;
+    }
+    index = index +1;
+}
+Console.WriteLine("--");
+Console.WriteLine(sum);
+
