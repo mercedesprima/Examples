2476c00583f9a26dafa81564b1299fe80669e93a
Ekaterina_P <zazie25@yandex.ru>
2023-01-17 23:57:41 +0300

    add Example 3

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿int numberA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+//выводится с новой строки
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
