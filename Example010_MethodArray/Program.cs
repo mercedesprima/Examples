61fdf31b4716c78b9caf3418afe361e2e3f514d5
Ekaterina_P <zazie25@yandex.ru>
2023-01-18 00:12:39 +0300

    add Example 10

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,9 +1,11 @@
-﻿int [] array = {1, 12, 31, 18, 4, 15, 16, 17 ,18};
+﻿// Объявляем массив
+int [] array = {1, 12, 31, 18, 4, 15, 16, 17 ,18};
 
 int n = array.Length;
 int find = 18;
 
 int index = 0;
+// цикл
 while (index < n)
 {
     if (array [index] == find)
