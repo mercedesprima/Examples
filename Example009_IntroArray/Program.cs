0c83c3cc8fdbce86a564b904025cade9ccf0545a
Ekaterina_P <zazie25@yandex.ru>
2023-01-18 00:10:58 +0300

    add Example 9

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -4,6 +4,7 @@
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
     return result;
+    // массив
 } //            0   1   2   3   4   5   6   7   8
 int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
 
